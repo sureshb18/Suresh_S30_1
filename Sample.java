80e97d9dc0d22ffc999f08de96d0f5d3d497c053
sureshb18 <sureshb.sapep@gmail.com>
2026-02-07 18:56:35 -0600

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,44 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : 
 
 
 // Your code here along with comments explaining your approach
+
+// Approach
+
+// 1) create boolean array of given size
+// 2) add true to boolean array if adding an element
+// 3) add false to boolean array if deleting
+// 4) return value of the key
+
+class MyHashSet {
+
+    private boolean[] data;
+
+    public MyHashSet() {
+        data = new boolean[1000001];
+        
+    }
+    
+    public void add(int key) {
+        data[key] = true;
+    }
+    
+    public void remove(int key) {
+        data[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        return data[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
